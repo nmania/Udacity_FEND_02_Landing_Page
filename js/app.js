17c6fb2538582d49875fa278e5be0fe16f2adb20
yukariIm8 <55606330+miirakuy@users.noreply.github.com>
2020-07-09 23:12:20 -0700

    feat: add nav, scroll, activeSection.

--- js/app.js
+++ js/app.js
@@ -18,6 +18,13 @@
  * 
 */
 
+/* global document */
+/* global window */
+
+const sections = document.querySelectorAll('section');
+const navList = document.getElementById('navbar__list');
+const navItems = [];
+
 
 /**
  * End Global Variables
@@ -25,6 +32,11 @@
  * 
 */
 
+//Extract section ids as navigation items.
+for (let i = 0; i < sections.length; i++) {
+    const section = sections[i];
+    navItems.push(section.getAttribute('id'));
+}
 
 
 /**
@@ -34,12 +46,58 @@
 */
 
 // build the nav
-
+const nav = () => {
+    for (let i = 1; i <= navItems.length; i++) {
+        const newList = document.createElement('li');
+        const newAnchor = document.createElement('a');
+        newAnchor.setAttribute('class', 'menu__link');
+        newAnchor.setAttribute('href', `#section${i}`);
+        newAnchor.textContent = `Section ${i}`;
+        newList.appendChild(newAnchor);
+        navList.appendChild(newList);
+    }
+};
 
 // Add class 'active' to section when near top of viewport
 
+const isInViewport = element => {
+    const position = element.getBoundingClientRect();
+    return (
+        position.top >= 0 &&
+        position.left >= 0 &&
+        position.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        position.right <= (window.innerWidth || document.documentElement.clientWidth)) ? true : false;
+};
+
+const activeSection = () => {
+    document.addEventListener('scroll', () => {
+        for (const section of sections) {
+            if (isInViewport(section)) {
+                section.classList.add('your-active-class');
+            } else {
+                section.classList.remove('your-active-class');
+            }
+        }
+    });
+};
+
 
 // Scroll to anchor ID using scrollTO event
+
+const scroll = () => {
+    document.addEventListener('click', e => {
+        const target = e.target;
+        if (!target.classList.contains('menu__link'))
+            return;
+        e.preventDefault();
+        const targetId = target.hash;
+        document.querySelector(targetId).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+            inline: 'nearest',
+        });
+    });
+};
 
 
 /**
@@ -49,9 +107,10 @@
 */
 
 // Build menu 
+nav();
 
 // Scroll to section on link click
+scroll();
 
 // Set sections as active
-
-
+activeSection();
